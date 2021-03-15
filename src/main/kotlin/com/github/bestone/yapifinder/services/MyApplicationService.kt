099b9cf8e35c1fa58890f5e268ae17d6d1d2511c
GitHub Action <action@github.com>
2021-03-15 11:15:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/bestone/yapifinder/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bestone/yapifinder/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bestone.yapifinder.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bestone.yapifinder.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/bestone/yapifinder/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bestone/yapifinder/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.bestone.yapifinder.services
 
+import com.github.bestone.yapifinder.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
